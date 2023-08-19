eeddb45d35b514883b0b60ce2c897c65492fd963
exsyss <exsysb@gmail.com>
2023-08-19 10:12:14 +0200

    added fade in animation

--- components/mint/section1.vue
+++ components/mint/section1.vue
@@ -1,14 +1,18 @@
 <template>
     <div class="h-full">
-        <div class="flex flex-col-reverse justify-end items-center lg:flex-row lg:justify-center h-full w-4/5 xl:w-3/4 mx-auto gap-10">
-            <div class="sm:w-[70%] lg:w-[40%] xl:mr-16">
+        <div class="flex flex-col-reverse justify-end items-center lg:flex-row lg:justify-center
+            h-full w-4/5 xl:w-3/4 mx-auto gap-10">
+            <div ref="target" class="fadeInFromLeft sm:w-[70%] lg:w-[40%] xl:mr-16" :class="isVisible ? 'visible' : ''">
                 <img src="~/assets/images/isabell.png" alt="anima" class="w-full">
             </div>
-            <div class="flex flex-col text-center mb-10 lg:text-left lg:max-w-[50%] xl:max-w-[35%]">
+            <div ref="target" class="fadeInFromRight flex flex-col text-center mb-10 lg:text-left lg:max-w-[50%] xl:max-w-[35%]"
+            :class="isVisible ? 'visible' : ''">
                 <h1 class="text-4xl mb-6">About RE:Anima</h1>
-                <div class="">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod fuga ab praesentium dolores nulla
-                    voluptatum harum minima iste quaerat repellendus earum debitis, eum eius, quos temporibus illum culpa ex?
+                <div>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod fuga ab praesentium dolores
+                    nulla
+                    voluptatum harum minima iste quaerat repellendus earum debitis, eum eius, quos temporibus illum culpa
+                    ex?
                 </div>
             </div>
         </div>
@@ -16,7 +20,25 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+import { useIntersectionObserver } from '@vueuse/core';
+
 export default {
     name: "section1",
+    setup() {
+        const target = ref(null);
+        const isVisible = ref(false);
+
+        const { stop } = useIntersectionObserver(
+            target,
+            ([{ isIntersecting }], observerElement) => {
+                isVisible.value = isIntersecting;
+                if (isIntersecting) stop();
+            },
+            { threshold: 0.6 }
+        );
+
+        return { target, isVisible };
+    },
 }
 </script>
--- components/mint/section2.vue
+++ components/mint/section2.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="h-full">
-        <div class="flex flex-col justify-end items-center lg:flex-row lg:justify-center h-full w-4/5 xl:w-3/4 mx-auto gap-10">
-            <div class="flex flex-col text-center mb-10 lg:text-left lg:max-w-[50%] xl:max-w-[35%]">
+        <div class="flex flex-col justify-end items-center lg:flex-row lg:justify-center 
+        h-full w-4/5 xl:w-3/4 mx-auto gap-10">
+            <div ref="target" class="fadeInFromLeft flex flex-col text-center mb-10 lg:text-left lg:max-w-[50%] xl:max-w-[35%]"
+            :class="isVisible ? 'visible' : ''">
                 <h1 class="text-4xl mb-6">Title</h1>
                 <div>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate tenetur deleniti, repellat corrupti
@@ -9,7 +11,7 @@
                     voluptas aut sapiente?
                 </div>
             </div>
-            <div class="w-[150%] sm:w-[90%] lg:w-[60%]">
+            <div ref="target" class="fadeInFromRight w-[150%] sm:w-[90%] lg:w-[60%]" :class="isVisible ? 'visible' : ''">
                 <img src="~/assets/images/group1.png" alt="anima" class="w-full">
             </div>
         </div>
@@ -17,7 +19,28 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+import { useIntersectionObserver } from '@vueuse/core';
+
 export default {
     name: "section2",
+    setup() {
+        const target = ref(null);
+        const isVisible = ref(false);
+
+        const { stop } = useIntersectionObserver(
+            target,
+            ([{ isIntersecting }], observerElement) => {
+                isVisible.value = isIntersecting;
+                if (isIntersecting) stop();
+            },
+            { threshold: 0.6 }
+        );
+
+        return { target, isVisible };
+    },
+    methods: {
+
+    }
 }
 </script>
