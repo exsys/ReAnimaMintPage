1d32fa52f5b9417c9891a6c0a5df9f4f7a73e325
exsyss <exsysb@gmail.com>
2023-08-25 10:18:54 +0200

    added token id tracker

--- components/mint/mintPage.vue
+++ components/mint/mintPage.vue
@@ -5,7 +5,8 @@
             px-4 py-10 bg-gray-700/80 lg:bg-transparent rounded-xl z-10 border-2 border-slate-500 lg:border-none">
                 <h1 class="text-5xl mb-8 font-medium">RE:Anima Pass</h1>
                 <h2 class="text-2xl">Some random info text</h2>
-                <div v-if="!whitelistMintActive && !publicMintActive && !mintEnded && !walletHasMinted" class="mt-6 flex flex-col w-full">
+                <div v-if="!whitelistMintActive && !publicMintActive && !mintEnded && !walletHasMinted"
+                    class="mt-6 flex flex-col w-full">
                     <div class="mb-6">
                         <h3 class="text-2xl font-medium">Time left:</h3>
                         <div class="text-3xl">
@@ -41,6 +42,9 @@
                     <button class="w-48 font-medium main-button rounded-md py-2 px-10" @click="mintPass">
                         Mint
                     </button>
+                    <div class="mt-6">
+                        <h3 class="text-3xl">{{ amountMinted }}/{{ maxAmount }}</h3>
+                    </div>
                 </div>
 
                 <div v-if="publicMintActive && !mintEnded && !walletHasMinted" class="mt-10">
@@ -50,6 +54,9 @@
                     <button class="w-48 font-medium main-button rounded-md py-2 px-10" @click="mintPass">
                         Mint
                     </button>
+                    <div class="mt-6">
+                        <h3 class="text-3xl">{{ amountMinted }}/{{ maxAmount }}</h3>
+                    </div>
                 </div>
 
                 <div v-if="mintEnded && !walletHasMinted" class="mt-6">
@@ -69,21 +76,22 @@
 <script>
 import { useOnboard } from '@web3-onboard/vue';
 import { ethers } from 'ethers';
-import { reAnimaPassContractAddress } from "@/data/constants";
+import { networkSettings, reAnimaPassContractAddress } from "@/data/constants";
 import { reAnimaPassABI } from '@/data/abis';
-import { mainChainId } from '@/data/constants';
 import { toast } from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
 import changeNetwork from "@/components/toastify/changeNetwork";
 
-const mainChainIdHex = `0x${mainChainId.toString(16)}`;
-const viewOnlyProvider = new ethers.JsonRpcProvider("https://arbitrum-goerli.public.blastapi.io");
-const reAnimaViewOnlyContract = new ethers.Contract(reAnimaPassContractAddress.testnet, reAnimaPassABI, viewOnlyProvider);
+const chainIdHex = networkSettings.testnet.chainInfo.id; // TODO: change to mainnet
+const viewOnlyProvider = new ethers.JsonRpcProvider(networkSettings.testnet.rpcUrl); // TODO: change to mainnet
+const reAnimaViewOnlyContract = new ethers.Contract(reAnimaPassContractAddress.testnet, reAnimaPassABI, viewOnlyProvider); // TODO: change to mainnet
+const amountMintedFilter = reAnimaViewOnlyContract.filters.Transfer(null, null);
 
 export default {
     name: "mintPage",
     data() {
         return {
+            maxAmount: 2222,
             days: 0,
             hours: 0,
             minutes: 0,
@@ -99,7 +107,7 @@ export default {
             eligible: false,
             eligibilityChecked: false,
             walletAddress: "",
-            amount: 0,
+            amountMinted: 0,
         }
     },
     watch: {
@@ -123,35 +131,39 @@ export default {
             },
             immediate: true,
         },
+        connectedWallet: async function (newVal, oldVal) {
+            if (newVal.accounts.length) {
+                try {
+                    const balance = Number(await reAnimaViewOnlyContract.balanceOf(this.connectedWallet.accounts[0].address));
+                    if (balance > 0) this.walletHasMinted = true;
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }
     },
     setup() {
         const { connectedWallet, wallets, alreadyConnectedWallets, connectedChain } = useOnboard();
         return { connectedWallet, wallets, alreadyConnectedWallets, connectedChain };
     },
     async mounted() {
-        const teamMinted = Number(await reAnimaViewOnlyContract.teamMinted());
-        const whiteListMinted = Number(await reAnimaViewOnlyContract.whiteListMinted());
-        if (teamMinted + whiteListMinted >= 2000) this.mintEnded = true;
         if (Date.now() >= this.whitelistStartTimestamp && Date.now() < this.whitelistEndTimestamp) this.whitelistMintActive = true;
         if (Date.now() >= this.whitelistEndTimestamp) this.publicMintActive = true;
         this.updateTimeLeft();
-        // TODO: check amount minted every 7s
+        await this.getInitalAmountMinted();
+        this.startTrackingLatestTokenId();
     },
     methods: {
-        changeAmount(byAmount) {
-            if (this.amount + byAmount < 0) return;
-            this.amount += byAmount;
-        },
         async mintPass() {
-            //const wrongNetwork = this.checkCorrectNetwork();
-            //if (wrongNetwork) return;
+            const wrongNetwork = this.checkCorrectNetwork();
+            if (wrongNetwork) return;
             if (this.connectedWallet) {
                 const ethersProvider = new ethers.BrowserProvider(this.connectedWallet.provider);
                 const signer = await ethersProvider.getSigner();
-                const reAnimaPassContract = new ethers.Contract(reAnimaPassContractAddress.testnet, reAnimaPassABI, signer);
+                const reAnimaPassContract = new ethers.Contract(reAnimaPassContractAddress.testnet, reAnimaPassABI, signer); // TODO: change to mainnet
                 try {
                     const tx = await reAnimaPassContract.mint();
-                    const balance = Number(await reAnimaPassABI.balanceOf(this.connectedWallet.accounts[0].address));
+                    const balance = Number(await reAnimaViewOnlyContract.balanceOf(this.connectedWallet.accounts[0].address));
                     if (balance > 0) this.walletHasMinted = true;
                 } catch (error) {
                     if (error.info.error.code === 4001) {
@@ -207,9 +219,60 @@ export default {
                 });
             }
         },
+        async getInitalAmountMinted() {
+            if (this.whitelistMintActive || this.publicMintActive) {
+                try {
+                    var currentBlock = await viewOnlyProvider.getBlockNumber();
+                    var startBlock = currentBlock - 1000;
+                    var endBlock = currentBlock;
+                } catch (error) {
+                    console.log(error);
+                }
+    
+                let latestTransferEvent;
+                while (!latestTransferEvent) {
+                    try {
+                        const transferEvents = await reAnimaViewOnlyContract.queryFilter(amountMintedFilter, startBlock, endBlock);
+                        if (transferEvents.length) {
+                            latestTransferEvent = transferEvents.slice(-1)[0];
+                        } else {
+                            startBlock -= 1000;
+                            endBlock -= 1000;
+                        }
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }
+                const latestTokenId = parseInt(latestTransferEvent.topics[3], 16);
+                this.amountMinted = latestTokenId;
+                if (latestTokenId >= this.maxAmount) this.mintEnded = true;
+            }
+        },
+        startTrackingLatestTokenId() {
+            if (this.whitelistMintActive || this.publicMintActive) {
+                setTimeout(async () => {
+                    try {
+                        const currentBlock = await viewOnlyProvider.getBlockNumber();
+                        const transferEvents = await reAnimaViewOnlyContract.queryFilter(amountMintedFilter, currentBlock - 1000);
+                        if (transferEvents.length) {
+                            const latestTransferEvent = transferEvents.slice(-1)[0];
+                            const latestTokenId = parseInt(latestTransferEvent.topics[3], 16);
+                            this.amountMinted = latestTokenId;
+                            if (latestTokenId >= this.maxAmount) {
+                                this.mintEnded = true;
+                                return; // if all tokens have minted out tracking will stop
+                            }    
+                        }
+                        setTimeout(this.startTrackingLatestTokenId, 10000); // track token id every 10 seconds
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }, 1000);
+            }
+        },
         checkCorrectNetwork() {
             if (this.connectedWallet) {
-                if (this.connectedChain.id !== mainChainIdHex) {
+                if (this.connectedChain.id !== chainIdHex) {
                     toast.warning(changeNetwork, {
                         autoClose: 7000,
                         position: toast.POSITION.TOP_CENTER,
